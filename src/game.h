f936249e0f22d8c9788346a2f211f96f85ece603
pontaberglund <60647163+pontaberglund@users.noreply.github.com>
2022-02-18 11:11:45 +0100

    Declare Oled functions globally

--- src/game.h
+++ src/game.h
@@ -3,3 +3,11 @@ int getButtons();
 
 //Enable interrupts with MIPS
 void enable_interrupts();
+
+//Oled functions
+void OledHostInit();
+void OledDspInit();
+void OledUpdate();
+
+//Declare scrrenbuffer
+extern unsigned char displayBuffer[512];

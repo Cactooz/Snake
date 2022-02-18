9f0797866b15ecb67ff1f544f93f79a3b1eefe4c
Cactooz <54435874+Cactooz@users.noreply.github.com>
2022-02-18 11:36:33 +0100

    Declare OLED buffer and updatePixel function

--- src/game.h
+++ src/game.h
@@ -3,3 +3,9 @@ int getButtons();
 
 //Enable interrupts with MIPS
 void enable_interrupts();
+
+//OLED functions
+void updatePixel(unsigned char x, unsigned char y, unsigned char bool);
+
+//Buffer for the OLED display
+extern unsigned char displayBuffer[512];

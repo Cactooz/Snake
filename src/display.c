2089ee1fe174addd5b9ab3e0a7eaebb48ac01b2f
Cactooz <54435874+Cactooz@users.noreply.github.com>
2022-02-18 10:24:03 +0100

    Add updatePixel function
    Function for turning a single pixel on or off

--- src/display.c
+++ src/display.c
@@ -2,3 +2,20 @@
 #include <pic32mx.h>
 #include "game.h"
 
+//Function for turning a single pixel on or off
+void updatePixel(unsigned char x, unsigned char y, unsigned char bool) {
+	//Check if the pixels are inside of the screen
+	if(x < 128 && y < 32 && bool <= 1) {
+		//Get the position in the array that should be changed
+		short pos = ((y/8)*128) + x;
+
+		//Turn on pixel if bool = 1
+		if(bool == 1) {
+			displayBuffer[pos] = (displayBuffer[pos] | (0x1 << (y%8)));
+		}
+		//Turn off pixel if bool = 0
+		else if(bool == 0) {
+			displayBuffer[pos] = (displayBuffer[pos] & ~(0x1 << (y%8)));
+		}
+	}
+}

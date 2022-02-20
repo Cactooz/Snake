e35f042d9a3f4d3a8e1c1cc3192a66ec54d72da5
pontaberglund <60647163+pontaberglund@users.noreply.github.com>
2022-02-20 15:00:14 +0100

    Add menu code to main

--- src/main.c
+++ src/main.c
@@ -30,19 +30,39 @@ int main() {
 	//Set the gameState to menu
 	enum gameState gameState = menu;
 
+	//Used for clearing display
+	int column;
+	int row;
+
 	//Run the game
 	while(1) {
 		switch(gameState) {
 			case menu:
-				runMenu();
+				switch(runMenu())
+				{
+					case 1:
+						//Clears the screen
+    					for (column = 0; column < 128; column++)
+        					for(row = 0; row < 32; row++)
+            					updatePixel(column, row, 0);
+
+						gameState = game;
+						break;
+					
+					case 2:
+						//Clears the screen
+    					for (column = 0; column < 128; column++)
+        					for(row = 0; row < 32; row++)
+            					updatePixel(column, row, 0);
+						gameState = highscore;
+						break;
+				}
 				break;
 			case game:
 				//Code for the game
 				//Move the snake
-				moveSnake();
-
-				//Draw the snake
-				drawSnake();
+				printWord("GAME", 4, 50, 12);
+				OledUpdate();
 				break;
 			case gameOver:
 				//Code for game over menu
@@ -50,7 +70,8 @@ int main() {
 				break;
 			case highscore:
 				//Code for highscore menu
-
+				printWord("HIGHSCORE", 9, 50, 12);
+				OledUpdate();
 				break;
 			default:
 				break;
